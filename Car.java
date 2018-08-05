893e2f28cf5544544410f612ef7a9eef1c6f9f25
OlenaTS <tsynda.elena@gmail.com>
2018-08-06 00:15:43 +0300

    Second commit

--- Car.java
+++ Car.java
@@ -1,5 +1,7 @@
+import java.util.Objects;
+
 public class Car {
-    // Task 2
+    // Task 3
     private String brand;
 
     public Car(String brand) {
@@ -26,5 +28,48 @@ public class Car {
             }
         }
         return new CarEngine();
+    }
+
+    // Task 4
+    public static IEngine creatingElectricCarEngine() {
+
+        return new IEngine() {
+
+            @Override
+            public void start() {
+                System.out.println("CarEngine is started.");
+            }
+
+            @Override
+            public void fly() {
+                System.out.println("Unfortunately, cars can't fly :(");
+            }
+
+            @Override
+            public void stop() {
+                System.out.println("CarEngine has stopped its work.");
+            }
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(brand, car.brand);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(brand);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                '}';
     }
 }
